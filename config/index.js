60381766160831d7bd6ac9f6a37d80b5c4179bc0
nguyen9112001 <tranthaonguyen911@gmail.com>
2021-08-03 14:38:12 +0700

    importTimeoutMongose

--- config/index.js
+++ config/index.js
@@ -1,14 +1,25 @@
 const mongoose = require('mongoose')
+const serverOptions = {
+    poolSize: 100,
+    socketOptions: {
+      socketTimeoutMS: 60000
+    }
+  };
+mongoose.createConnection(dbpath, {
+    server: serverOptions,
+    replset: serverOptions
+  });
  async function connect(){
     try{
         await mongoose.connect('mongodb://localhost:27017/API_SHOP',{
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
         });
         console.log('connect successfully!!!')
     }catch (error) {
         console.log('connect faillue !!!')
     }
  }
+ 
  
  module.exports = {connect}
