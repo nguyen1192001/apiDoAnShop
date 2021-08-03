123ffb0a697b5f739f5ee7049e5bf76cfdafca29
nguyen9112001 <tranthaonguyen911@gmail.com>
2021-08-03 13:56:08 +0700

    finderrorh12

--- index.js
+++ index.js
@@ -13,9 +13,13 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
      next();
 });
-app.use('/product',productRoute)
-app.use('/user',productUser)
-app.use('/cart',productCart)
+function checkUserAuth(req, res, next) {
+  if (req.session.user) return next();
+  return next(new NotAuthorizedError());
+}
+app.use('/product',checkUserAuth,productRoute)
+app.use('/user',checkUserAuth,productUser)
+app.use('/cart',checkUserAuth,productCart)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
