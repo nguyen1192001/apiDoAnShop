9cd3488527b9cceebec2c7f9d6f2d286a459933d
nguyen9112001 <tranthaonguyen911@gmail.com>
2021-08-03 15:58:28 +0700

    again

--- config/index.js
+++ config/index.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose')
-const serverOptions = {
-    poolSize: 100,
-    socketOptions: {
-      socketTimeoutMS: 60000
-    }
-  };
-mongoose.createConnection(dbpath, {
-    server: serverOptions,
-    replset: serverOptions
-  });
+
  async function connect(){
     try{
         await mongoose.connect('mongodb://localhost:27017/API_SHOP',{
