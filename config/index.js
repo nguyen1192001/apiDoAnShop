e7cbe4c74296e59060ab07db78c4d24262898f92
unknown <tranthaonguyen911@gmail.com>
2022-05-11 08:51:29 +0700

    pushlan3

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
  async function connect(){
     try{
-        await mongoose.connect('mongodb://localhost:27017/DBGiaoThucAnNhanh',{
+        await mongoose.connect('mongodb+srv://ThaoNguyen:123@cluster0.5ak4b.mongodb.net/DBGiaoThucAnNhanh?retryWrites=true&w=majority',{
             useNewUrlParser: true,
             useUnifiedTopology: true,
           
